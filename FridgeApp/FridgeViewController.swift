385227f421f4f471edad3ac3ed95391741ee707c
biaplutarco <biaplutarco@gmail.com>
2019-08-24 12:21:52 -0300

    add collection view

--- FridgeApp/FridgeViewController.swift
+++ FridgeApp/FridgeViewController.swift
@@ -11,8 +11,52 @@ import UIKit
 class FridgeViewController: UIViewController {
     weak var coordinator: MainCoordinator?
     
+    lazy var collectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: flowLayout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .clear
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.AppColors.lightGray
+        addSubviews()
+        configConstrints()
     }
+    
+    func configConstrints() {
+        NSLayoutConstraint.activate([
+            collectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            collectionView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            collectionView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            collectionView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.7)
+            ])
+    }
+    
+    func addSubviews() {
+        view.addSubview(collectionView)
+    }
+}
+
+extension FridgeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath)
+        myCell.backgroundColor = UIColor.AppColors.red
+        return myCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 90)
+    }
+    
+    
 }

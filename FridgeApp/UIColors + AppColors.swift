f8806b22cdef068ed3320e51f50f8afb2beacde0
biaplutarco <biaplutarco@gmail.com>
2019-08-24 10:22:04 -0300

    add app colors

---
+++ FridgeApp/UIColors + AppColors.swift
@@ -0,0 +1,17 @@
+//
+//  UIColors + AppColors.swift
+//  FridgeApp
+//
+//  Created by Bia on 24/08/19.
+//  Copyright © 2019 akhaten. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    struct AppColors {
+        static let red = UIColor.init(named: "Red")
+        static let lightRed = UIColor.init(named: "LightRed")
+        static let lightGray = UIColor.init(named: "LightGray")
+    }
+}
